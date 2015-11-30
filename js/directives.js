f0b913a1050e848e6b44ea8ceb40cb71f63bed65
Ivy Chang <ivycwq@icloud.com>
2015-11-29 16:45:37 -0800

    adding caffeine meter

--- js/directives.js
+++ js/directives.js
@@ -11,3 +11,25 @@ app.directive("ivyTeaInfo", function(){
     }
   };
 });
+
+app.directive("caffeineScale",function(){
+  return{
+    restrict: "A",
+    templateUrl: "templates/caffeineScale.html",
+    scope: {
+      caffeinemg: "@"
+    },
+    link: function(scope, element, attrs){
+      if(attrs.caffeinemg < 100){
+        element.addClass("low");
+        scope.level = "low";
+      }else if(attrs.caffeinemg > 100 && attrs.caffeinemg < 200){
+        scope.level = "medium";
+        element.addClass("medium");
+      }else{
+        element.addClass("high");
+        scope.level = "high";
+      }
+    }
+  };
+});
